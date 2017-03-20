0b0817b41f0dfa58240e1d3b16a8eafecf8f49bd
Christian Wunder <wunderman4@gmail.com>
2017-03-20 14:59:29 -0700

    Added Manage and Edit Players Page. Not totally functioning yet.

---
+++ TableTennisTracker/EditPlayer.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TableTennisTracker.Models;
+using TableTennisTracker.Services;
+
+namespace TableTennisTracker
+{
+    /// <summary>
+    /// Interaction logic for EditPlayer.xaml
+    /// </summary>
+    public partial class EditPlayer : Page
+    {
+        PlayerService ps = new PlayerService();
+        Player Player = null;
+        
+        
+
+        public EditPlayer(Player p)
+        {
+            InitializeComponent();
+            Player = p;
+
+            // sets page Data Context. 
+            this.DataContext = this;
+            UserNameTextBox.DataContext = Player;
+            NameTextBox.DataContext = Player;
+            HeightFt.DataContext = Player;
+            HeightIn.DataContext = Player;
+            AgeTextBox.DataContext = Player;
+            PPH.DataContext = Player;
+            CountryTextBox.DataContext = Player;
+
+        }
+
+        private async void CancelEdit(object sender, RoutedEventArgs e)
+        {
+            await Task.Delay(350);
+            NavigationService.Navigate(new ManagePlayers());
+        }
+
+        // Checks required fields, converts data type, Assigns to type User then adds to database.
+        private async void Submit(object sender, RoutedEventArgs e)
+        {
+            // Checks required fields
+            if (AgeTextBox.Text == "" ||
+                UserNameTextBox.Text == "" ||
+                NameTextBox.Text == "" ||
+                HeightFt.Text == "" ||
+                HeightIn.Text == "" ||
+                PPH.Text == "" ||
+                CountryTextBox.Text == "")
+            {
+                // SnackBar Popup if Feilds not filled in.
+                EnterAllFieldsError.IsActive = true;
+                await Task.Delay(3000);
+                EnterAllFieldsError.IsActive = false;
+            }
+            else
+            {
+
+                // Converts DataTypes 
+                int age = Convert.ToInt32(AgeTextBox.Text);
+                int heightFeet = Convert.ToInt32(HeightFt.Text.ToString());
+                int heightInches = Convert.ToInt32(HeightIn.Text.ToString());
+
+                // Updates Player
+                Player.UserName = UserNameTextBox.Text;
+                Player.PlayerName = NameTextBox.Text;
+                Player.Age = age;
+                Player.HeightFt = heightFeet;
+                Player.HeightInch = heightInches;
+                Player.Nationality = CountryTextBox.Text;
+                Player.HandPreference = PPH.Text;
+
+                // Updates Database
+                ps.UpdatePlayer(Player);
+
+                // Navigation Back to Manage Players Page
+                await Task.Delay(200);
+                NavigationService.Navigate(new ManagePlayers());
+            }
+        }
+    }
+}
---
+++ TableTennisTracker/ManagePlayers.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TableTennisTracker.Models;
+using TableTennisTracker.Services;
+
+namespace TableTennisTracker
+{
+    /// <summary>
+    /// Interaction logic for ManagePlayers.xaml
+    /// </summary>
+    public partial class ManagePlayers : Page
+    {
+        PlayerService ps = new PlayerService();
+        List<Player> PlayerList;
+        Player Player = null;
+
+        public ManagePlayers()
+        {
+            InitializeComponent();
+            GetPlayers();
+        }
+
+        // Gets the list of players
+        private void GetPlayers()
+        {
+            // Gets list of players
+            PlayerList = ps.ListPlayers();
+
+            // assigns each list to the listbox on xaml page. 
+            PlayerListBox.ItemsSource = PlayerList;
+        }
+
+        // Edit Player
+        private void EditPlayer(object sender, RoutedEventArgs e)
+        {
+             
+            foreach (Player p in PlayerListBox.SelectedItems)
+            {
+                    Player = p;
+            }
+
+            NavigationService.Navigate(new EditPlayer(Player));
+
+        }
+
+        private async void DeleteClick(object sender, RoutedEventArgs e)
+        {
+
+            ConfirmDelete.Visibility = Visibility.Visible;
+            await Task.Delay(4000);
+            ConfirmDelete.Visibility = Visibility.Collapsed;
+
+        }
+    }
+}
