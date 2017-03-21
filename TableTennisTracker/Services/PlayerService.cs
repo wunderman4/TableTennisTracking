9b3174f851d3fefdccd0f8c46104ecc7d6c4b600
Richard Charay <richardcharay@gmail.com>
2017-03-21 13:08:10 -0700

    Fixed PlayersWithGames

--- TableTennisTracker/Services/PlayerService.cs
+++ TableTennisTracker/Services/PlayerService.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using TableTennisTracker.Interfaces;
 using TableTennisTracker.Models;
 using TableTennisTracker.Respository;
+using TableTennisTracker.Views;
 
 namespace TableTennisTracker.Services
 {
@@ -28,11 +29,77 @@ namespace TableTennisTracker.Services
             return playerList;
         }
 
+        public bool PlayerHasGames(int id)
+        {
+            List<Game> games = (from gp in _repo.Query<GamePlayer>()
+                                where gp.PlayerId == id
+                                select gp.Game).ToList();
+            if (games.Count() == 0)
+                return false;
+            else
+                return true;
+        }
+        public List<PlayerWithGames> ListPlayersWithGames()
+        {
+            List<PlayerWithGames> playersWtihGames = (from p in _repo.Query<Player>()
+                                                      select new PlayerWithGames
+                                                      {
+                                                          Id = p.Id,
+                                                          UserName = p.UserName,
+                                                          PlayerName = p.PlayerName,
+                                                          Age = p.Age,
+                                                          HeightFt = p.HeightFt,
+                                                          HeightInch = p.HeightInch,
+                                                          Nationality = p.Nationality,
+                                                          HandPreference = p.HandPreference,
+                                                          IsSelected = p.IsSelected,
+                                                          Wins = p.Wins,
+                                                          Losses = p.Losses
+                                                      }).ToList();
+
+            foreach(PlayerWithGames player in playersWtihGames)
+            {
+                List<Game> games = (from gp in _repo.Query<GamePlayer>()
+                                    where gp.PlayerId == player.Id
+                                    select gp.Game).ToList();
+                player.Games = games;
+            }
+
+            return playersWtihGames;
+        }
         public Player GetPlayer(int id)
         {
             return (from p in _repo.Query<Player>()
                     where p.Id == id
                     select p).FirstOrDefault();
+        }
+
+        public PlayerWithGames GetPlayerWithGames(int id)
+        {
+            PlayerWithGames playerWithGames = (from p in _repo.Query<Player>()
+                                               where p.Id == id
+                                               select new PlayerWithGames
+                                               {
+                                                   Id = p.Id,
+                                                   UserName = p.UserName,
+                                                   PlayerName = p.PlayerName,
+                                                   Age = p.Age,
+                                                   HeightFt = p.HeightFt,
+                                                   HeightInch = p.HeightInch,
+                                                   Nationality = p.Nationality,
+                                                   HandPreference = p.HandPreference,
+                                                   IsSelected = p.IsSelected,
+                                                   Wins = p.Wins,
+                                                   Losses = p.Losses
+                                               }).FirstOrDefault();
+
+            List<Game> games = (from gp in _repo.Query<GamePlayer>()
+                          where gp.PlayerId == id
+                          select gp.Game).ToList();
+
+            playerWithGames.Games = games;
+
+            return playerWithGames;
         }
 
         public void AddPlayer(Player newPlayer)
---
+++ TableTennisTracker/Views/PlayerWithGames.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TableTennisTracker.Models;
+
+namespace TableTennisTracker.Views
+{
+    public class PlayerWithGames
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PlayerName { get; set; }
+        public int Age { get; set; }
+        public int HeightFt { get; set; }
+        public int HeightInch { get; set; }
+        public string Nationality { get; set; }
+        public string HandPreference { get; set; }
+        public bool IsSelected { get; set; } = false;
+        public int Wins { get; set; }
+        public int Losses { get; set; }
+
+        public List<Game> Games { get; set; }
+    }
+}
