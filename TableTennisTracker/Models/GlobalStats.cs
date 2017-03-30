13aca7683e62619924d70251ff86994392538b5e
Richard Charay <richardcharay@gmail.com>
2017-03-30 09:28:52 -0700

    Modified db

---
+++ TableTennisTracker/Models/GlobalStats.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TableTennisTracker.Models
+{
+    public class GlobalStats
+    {
+        public int Id { get; set; }
+        public string PlayerWithMostWins { get; set; }
+        public int PlayerWithMostWinsId { get; set; }
+        public int MostWins { get; set; }
+        public string PlayerWithMostGames { get; set; }
+        public int PlayerWithMostGamesId { get; set; }
+        public int MostGamesPlayed { get; set; }
+        public string PlayerWithBestWinRatio { get; set; }
+        public int PlayerWithBestWinRatioId { get; set; }
+        public float BestWinRatio { get; set; }
+        public string PlayerWithGreatestAvgPointSpreadWins { get; set; }
+        public int PlayerWithGreatestAvgPointSpreadWinsId { get; set; }
+        public float BestAvgPointSpreadWins { get; set; }
+        public string PlayerWithLeastAvgPointSpreadLosses { get; set; }
+        public int PlayerWithLeastAvgPointSpreadLossesId { get; set; }
+        public float LeastAvgPointSpreadLosses { get; set; }
+        public int GameWithLongestVolleyHits { get; set; }
+        public string Player1GameWithLongestVolleyHits { get; set; }
+        public int Player1GameWithLongestVolleyHitsId { get; set; }
+        public string Player2GameWithLongestVolleyHits { get; set; }
+        public int Player2GameWithLongestVolleyHitsId { get; set; }
+        public int LongestVolleyHits { get; set; }
+        public int GameWithLongestVolleyTime { get; set; }
+        public string Player1GameWithLongestVolleyTime { get; set; }
+        public int Player1GameWithLongestVolleyTimeId { get; set; }
+        public string Player2GameWithLongestVolleyTime { get; set; }
+        public int Player2GameWithLongestVolleyTimeId { get; set; }
+        public float LongestVolleyTime { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime? LastUpdated { get; set; }
+        
+    }
+}
