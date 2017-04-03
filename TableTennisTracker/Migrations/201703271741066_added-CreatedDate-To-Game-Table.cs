694639eb4b0d1fc33b1201d69c320365c6815038
Richard Charay <richardcharay@gmail.com>
2017-04-03 10:22:18 -0700

    Commiting migration files

---
+++ TableTennisTracker/Migrations/201703271741066_added-CreatedDate-To-Game-Table.cs
@@ -0,0 +1,18 @@
+namespace TableTennisTracker.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addedCreatedDateToGameTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Games", "CreatedDate", c => c.DateTime(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Games", "CreatedDate");
+        }
+    }
+}
