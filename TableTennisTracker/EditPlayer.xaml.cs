c5de2c4fa8e9cc0bcd7a28a1eb72918a0bfbc5b9
Matthew Ploor <mploor@cox.net>
2017-04-05 11:54:52 -0700

    Fixed edit page height display, minor biography edits

--- TableTennisTracker/EditPlayer.xaml.cs
+++ TableTennisTracker/EditPlayer.xaml.cs
@@ -44,6 +44,87 @@ namespace TableTennisTracker
             PPH.DataContext = Player;
             CountryTextBox.DataContext = Player;
 
+            ShowHeightFt(Player.HeightFt);
+            ShowHeightIn(Player.HeightInch);
+            ShowHand(Player.HandPreference);
+        }
+
+        public void ShowHeightFt(int height)
+        {
+            switch (height)
+            {
+                case 4:
+                    H4.IsSelected = true;
+                    break;
+                case 5:
+                    H5.IsSelected = true;
+                    break;
+                case 6:
+                    H6.IsSelected = true;
+                    break;
+                case 7:
+                    H7.IsSelected = true;
+                    break;
+            }
+        }
+
+        public void ShowHeightIn(int height)
+        {
+            switch (height)
+            {
+                case 0:
+                    I0.IsSelected = true;
+                    break;
+                case 1:
+                    I1.IsSelected = true;
+                    break;
+                case 2:
+                    I2.IsSelected = true;
+                    break;
+                case 3:
+                    I3.IsSelected = true;
+                    break;
+                case 4:
+                    I4.IsSelected = true;
+                    break;
+                case 5:
+                    I5.IsSelected = true;
+                    break;
+                case 6:
+                    I6.IsSelected = true;
+                    break;
+                case 7:
+                    I7.IsSelected = true;
+                    break;
+                case 8:
+                    I8.IsSelected = true;
+                    break;
+                case 9:
+                    I9.IsSelected = true;
+                    break;
+                case 10:
+                    I10.IsSelected = true;
+                    break;
+                case 11:
+                    I11.IsSelected = true;
+                    break;
+            }
+        }
+
+        public void ShowHand(string hand)
+        {
+            switch (hand)
+            {
+                case "RIGHTY":
+                    RIGHTY.IsSelected = true;
+                    break;
+                case "SOUTH PAW":
+                    SOUTHPAW.IsSelected = true;
+                    break;
+                case "AMBIDEXTROUS":
+                    AMBIDEXTROUS.IsSelected = true;
+                    break;
+            }
         }
 
         private async void CancelEdit(object sender, RoutedEventArgs e)
